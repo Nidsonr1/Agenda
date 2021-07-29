e4c628b56786ffd7f6dfc48ef58e3c5cc0b6a658
Nidsonr1 <nidsoncampos@hotmail.com>
2021-07-29 15:35:05 -0300

    Adiçao dos controllers de login

--- src/controller/contatoController.js
+++
@@ -1,3 +0,0 @@
-exports.paginaInicial = (req, res, next) => {
-  res.send(`Obrigado por entrar em Contato.`)
-};
--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,11 +1,3 @@
-exports.paginaInicial = (req, res, next) => {
-  res.render('index', {
-    titulo: 'Este será o título da página',
-    numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  });
-  next();
+exports.index = (req, res, next) => {
+  res.render('index');
 };
-
-exports.trataPost = (req, res, next) => {
-  res.send(req.body)
-}
