def225011fd4f9e8eb652b51c5924d176984271b
Nidsonr1 <nidsoncampos@hotmail.com>
2021-07-29 15:37:04 -0300

    Fixed bugs

--- routes.js
+++ routes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const route = express.Router();
 
 const homeController = require('./src/controller/homeController');
-const contatoController = require('./src/controller/contatoController');
+const loginController = require('./src/controller/loginController');
 
 
 //Rotas Home
-route.get('/', homeController.paginaInicial);
-route.post('/', homeController.trataPost)
+route.get('/', homeController.index);
 
-//Rotas Contato
-route.get('/contato', contatoController.paginaInicial)
+//Rotas Login
+route.get('/login', loginController.index);
+route.post('/authentication/register', loginController.register)
+
 module.exports = route;
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,16 +1,14 @@
 exports.middlewareGlobal = (req, res, next) => {
- res.locals.umaVariavelLocal = 'Este é o valor da variável local.'; 
-  next()
-};
-
-exports.outroMiddleware = (req, res, next) => {
-  next()
+ res.locals.errors = req.flash('errors'); 
+ res.locals.success = req.flash('success');
+  next();
 };
 
 exports.chechCsrfError = (err, req, res, next) => {
-  if(err && err.code === 'EBADCSRFTOKEN') {
+  if(err) {
     return res.render('404');
   }
+  next();
 };
 
 exports.crsfMiddleware = (req, res, next) => {
