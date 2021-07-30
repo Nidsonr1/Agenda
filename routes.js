0f8a6297c87c1c01802528496c13fc53aa462874
Nidsonr1 <nidsoncampos@hotmail.com>
2021-07-30 12:26:23 -0300

    Adicionando rotas para login e logout de usuarios

--- routes.js
+++ routes.js
@@ -11,5 +11,7 @@ route.get('/', homeController.index);
 //Rotas Login
 route.get('/login', loginController.index);
 route.post('/authentication/register', loginController.register)
+route.post('/authentication/login', loginController.login)
+route.get('/authentication/logout', loginController.logout)
 
 module.exports = route;
