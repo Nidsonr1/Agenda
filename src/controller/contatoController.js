e160940435a2fbb2e9d0d4696a7e0c3632e73bc5
Nidsonr1 <nidsoncampos@hotmail.com>
2021-08-25 19:30:46 -0300

    Ajuste de bug no editar

--- src/controller/contatoController.js
+++ src/controller/contatoController.js
@@ -43,17 +43,18 @@ exports.edit = async (req, res) => {
     if(!req.params.id) return res.render(404);
     const contato = new Contato(req.body);
     await contato.edit(req.params.id);
-
+    
     if(contato.errors.length > 0) {
       req.flash('errors', contato.errors);
       req.session.save(() => { res.redirect('/contato') });
       return;
     }
     req.flash('success', 'Contato Editado com sucesso!');
-    req.session.save(() => { res.redirect(`/contato/${contato.contato._id}`) });
-    return;
-  } catch (error) {
+    req.session.save(() => { 
+      res.redirect(`/contato/${contato.contato._id}`) 
+    });
+  } catch (e) {
     console.log(e);
-    res.render('404');
+    res.render(404);
   }
 };
